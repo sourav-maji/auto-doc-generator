97109bfd5968c3ef6c9a6a6ddc82b0c3b2a336a3
sourav-maji <s722143@gmail.com>
2025-07-12 19:59:14 +0530

    testing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,6 @@ router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/', createUser);
 router.put('/:id', updateUser);
-// router.delete('/:id', deleteUser);
+router.delete('/:id', deleteUser);
 
 export default router;
